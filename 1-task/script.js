bb6d6ac921b49a3d12fe091155f7b7568afdbd17
Abdulazizjoon <xakimovxadyatillo92@gmail.com>
2024-12-06 16:14:35 +0500

    .

--- 1-task/script.js
+++ 1-task/script.js
@@ -1,27 +1,27 @@
-// let input = document.querySelector("input");
-// let btn = document.querySelector("button");
-// let wraper = document.querySelector(".wraper");
+let input = document.querySelector("input");
+let btn = document.querySelector("button");
+let wraper = document.querySelector(".wraper");
 
-// btn.addEventListener("click", function () {
-//   let randon = Math.trunc(Math.random() * 100);
-// console.log(randon);
-//   fetch(`http://numbersapi.com/${randon}?json`, {
-//     method: "GET"
-//   })
-//     .then((response) => {
-//       if (response.ok) {
-//         return response.json()
-//       }
-//       throw new Error("Tarmoqdan javob topilmadi");
-//     })
-//     .then((data) => {
-//       console.log(data);
-//       wraper.innerHTML += `<p>${data.text}</p>`;
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//       wraper.innerHTML += `<p>Xatolik: ${err.message}</p>`;
-//     })
-// })
+btn.addEventListener("click", function () {
+  let randon = Math.trunc(Math.random() * 100);
+console.log(randon);
+  fetch(`http://numbersapi.com/${randon}?json`, {
+    method: "GET"
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json()
+      }
+      throw new Error("Tarmoqdan javob topilmadi");
+    })
+    .then((data) => {
+      console.log(data);
+      wraper.innerHTML += `<p>${data.text}</p>`;
+    })
+    .catch((err) => {
+      console.log(err);
+      wraper.innerHTML += `<p>Xatolik: ${err.message}</p>`;
+    })
+})
 
 
